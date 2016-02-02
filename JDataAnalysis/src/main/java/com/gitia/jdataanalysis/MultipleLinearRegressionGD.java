7a136d5055a754f63be25b8e9617ee99ff0e6aa4
M <M@M-Pc>
2016-02-02 18:53:52 -0300

    -- 4.2.2 -- Modificamos el sistema para que trabaje por comandos Algortimo ELM - Agregamos Sistema de Datos Algoritmo Backpropagation - Agregamos Sistema de Datos

---
+++ JDataAnalysis/src/main/java/com/gitia/jdataanalysis/MultipleLinearRegressionGD.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2016 @author Matías Roodschild <mroodschild@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.gitia.jdataanalysis;
+
+import org.ejml.simple.SimpleMatrix;
+
+/**
+ *
+ * @author @author Matías Roodschild <mroodschild@gmail.com>
+ */
+public class MultipleLinearRegressionGD extends MultipleLinearRegression {
+
+    SimpleMatrix n;//step
+    double tolerance = 0.01;//stop criteria
+    SimpleMatrix minus2;
+
+    public MultipleLinearRegressionGD() {
+    }
+
+    public MultipleLinearRegressionGD(double[][] input, double[] output) {
+        H = new SimpleMatrix(input);
+        h = H.transpose();
+        W = new SimpleMatrix(input[0].length, 1);
+        n = new SimpleMatrix(input[0].length, 1, true, 0.01);
+        W.zero();
+        minus2 = new SimpleMatrix(input[0].length, 0, true, output);
+        Yobs = new SimpleMatrix(output.length, 1, true, output);
+        Yest = H.mult(W);
+        double rss = getRSS(H, Yobs);
+        System.out.println("Initial RSS: " + rss);
+        adjustW();
+        System.out.println("Finish RSS: " + getRSS(H, Yobs));
+    }
+
+    @Override
+    public void adjustW() {
+        while (gradRSS().normF() > 0.01) {
+            W = W.minus(gradRSS().mult(n));// W(old) - n * gradRSS()
+        }
+    }
+
+    /**
+     * output vertical vector
+     *
+     * @return - 2 Ht * ( Yobs - H * w)
+     */
+    public SimpleMatrix gradRSS() {
+        
+        SimpleMatrix gradRSS = H.transpose().mult(Yobs.minus(H.mult(W))).plus(-2);
+        return gradRSS;
+    }
+
+}
