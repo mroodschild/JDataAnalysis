82bffee6677d4250e4c606cfea947fab69e76cff
Matías Roodschild <mroodschild@gmail.com>
2017-06-06 19:58:41 -0300

    -- Updated License

--- JDataAnalysis/src/main/java/com/gitia/jdataanalysis/JDataAnalysis.java
+++ JDataAnalysis/src/main/java/com/gitia/jdataanalysis/JDataAnalysis.java
@@ -31,7 +31,6 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
