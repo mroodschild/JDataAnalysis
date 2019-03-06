761e5500f3ec53ac7a7a1fa7a7777677ea2c28cd
Matias Roodschild <mroodschild@gmail.com>
2019-03-05 23:29:23 -0300

    NormalizeDiv

---
+++ src/main/java/org/gitia/jdataanalysis/data/NormalizeDiv.java
@@ -0,0 +1,52 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2019
+ *   Matías Roodschild <mroodschild@gmail.com>.
+ *   Jorge Gotay Sardiñas <jgotay57@gmail.com>.
+ *   Adrian Will <adrian.will.01@gmail.com>.
+ *   Sebastián Rodriguez <sebastian.rodriguez@gitia.org>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.gitia.jdataanalysis.data;
+
+/**
+ *
+ * @author Matías Roodschild <mroodschild@gmail.com>
+ */
+public class NormalizeDiv {
+
+    /**
+     * dividimos los valores por el numero deseado
+     *
+     * @param data
+     * @return
+     */
+    public double[][] normalize(double[][] data, double div) {
+        double aux[][] = new double[data.length][data[0].length];
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data[0].length; j++) {
+                aux[i][j] = data[i][j] / div;
+            }
+        }
+        return aux;
+    }
+
+}
